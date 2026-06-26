71e0d28ac7add3b48defde8cf958e083838dbc39
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-06-26 12:14:57 +0300

    [CB/feat]: Load assets. Update structure of Preloader.

--- src/js/PreloaderScene.js
+++ src/js/PreloaderScene.js
@@ -1,7 +1,21 @@
 import * as PIXI from "pixi.js"
 import { sound } from "@pixi/sound";
+import { Assets } from "@pixi/assets";
+
+// PNG
+import hand from "../img/interface/Hand.png";
+import cell from "../img/interface/ImageFrame.png";
+import coverA from "../img/covers/Anime.png";
+import coverB from "../img/covers/Animals.png";
+import coverC from "../img/covers/Fantasy.png";
+import coverD from "../img/covers/Mandalas.png";
+
 import logoI from '../img/logo.png';
+
+// Sound
 import sound_fxA from "../audio/sound_fx.mp3";
+
+// Font
 import { fontUrl } from "../constants/font";
 
 let app;
@@ -11,14 +25,32 @@ export default class Preloader {
         app = application
     }
 
-    startLoad() {
-        console.log('%c  %c PreloaderScene ', 'background:#d6cc28','color: #d6cc28; background: #000; font-size:10pt')
+    async loadFont() {
+        try {
+            const font = new FontFace('Poppins', fontUrl);
+            await font.load();
+            document.fonts.add(font);
+            console.log('✅ Шрифт Poppins загружен');
+        } catch (e) {
+            console.warn('⚠️ Ошибка загрузки шрифта:', e);
+        }
+    }
 
+    loadPNG() {
         app.loader
-            .add('logo', logoI)
-        
+            .add('hand', hand)
+            .add('cell', cell)
+            .add("coverA", coverA)
+            .add("coverB", coverB)
+            .add("coverC", coverC)
+            .add("coverD", coverD);
+    }
+
+    loadSound() {
         sound
             .add('sound_fx', sound_fxA)
+    }
+
     async startLoad() {
         console.log('%c  %c PreloaderScene ', 'background:#d6cc28','color: #d6cc28; background: #000; font-size:10pt')
 
@@ -30,6 +62,13 @@ export default class Preloader {
         } catch (e) {
             console.warn('⚠️ Ошибка загрузки шрифта:', e);
         }
+
+        this.loadPNG();
+        
+        await this.loadFont();
+
+        //this.loadSound();
+
         return new Promise((resolve) => {
             resolve();
         });
