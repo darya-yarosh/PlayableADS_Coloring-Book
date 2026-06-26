62b0833db3c586c13399ea81d40b13f88ec4a511
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-06-26 12:20:50 +0300

    [CB/feat]: Add header and footer text for MainScene.
    * Remove logo.

--- src/js/MainScene.js
+++ src/js/MainScene.js
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js'
 import { sound } from '@pixi/sound';
-import { Sprite  } from '@pixi/sprite';
+import { Sprite } from '@pixi/sprite';
 
 import { gsap } from "gsap";
 import { PixiPlugin } from "gsap/PixiPlugin";
@@ -12,20 +12,32 @@ export default class Main {
     constructor (application) {
         app = application;
     }
+    
     startGame() {
-            console.log('%c  %c MainScene ', 'background:#219039','color: #219039; background: #000; font-size:10pt')
+        console.log('%c  %c MainScene ', 'background:#219039','color: #219039; background: #000; font-size:10pt')
 
-            const logo = Sprite .from('logo')
-            logo.anchor.set(0.5) 
-            app.stage.addChild(logo)
-    
-            logo.x = app.screen.width * 0.5
-            logo.y = app.screen.height * 0.5
-    
-            gsap.to(logo.scale, {
-                x: 1.1, y: 1.1, duration: 0.5, repeat: -1, yoyo: true, ease: 'Quad.InOut'
-            })
+        // -> Header
+        const headerText = new PIXI.Text("Choose category", {
+            fontFamily: 'Poppins',
+            fontSize: 24, fill : 0xff1010, align : 'center'
+        });
+        headerText.anchor.set(0.5, 0.5);
+        headerText.x = app.screen.width / 2;
+        headerText.y = 0 + 40 + headerText.height / 2;
+        app.stage.addChild(headerText);
+        // Header <-
 
-            music =  sound.play('sound_fx');
+
+        // -> Footer
+        const footerText = new PIXI.Text("Happy Color", {fontFamily: 'Poppins'});
+        footerText.anchor.set(0.5, 0.5);
+        footerText.x = app.screen.width / 2;
+        footerText.y = app.screen.height - 40 - footerText.height / 2;
+        app.stage.addChild(footerText);
+        // Footer <-
+
+        // -> Sound
+        // music =  sound.play('sound_fx');
+        // Sound <-
     }
 }
