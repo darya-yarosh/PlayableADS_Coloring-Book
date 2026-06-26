3f4396023d937f5229339d191debe3f67bf170be
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-06-26 12:22:54 +0300

    [CB/feat]: Create constant for levels-data.

---
+++ src/constants/levels.js
@@ -0,0 +1,31 @@
+export const LEVELS = [
+    {
+        cover: "coverA",
+        level: "levelA",
+        label: "labelA",
+    },
+    {
+        cover: "coverB",
+        level: "levelB",
+        label: "labelB",
+    },
+    {
+        cover: "coverC",
+        level: "levelC",
+        label: "labelC",
+    },
+    {
+        cover: "coverD",
+        level: "levelD",
+        label: "labelD",
+    },
+]
+
+// const cellType = [
+//     app.svgTextures?.typeA,
+//     app.svgTextures?.typeB,
+//     app.svgTextures?.typeC,
+//     app.svgTextures?.typeD,
+// ];
+
+// app.svgTextures?.[`${LEVELS[index].level}`]
