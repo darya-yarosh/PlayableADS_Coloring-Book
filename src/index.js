fd639b96dedef61b4e10562b950a95401919e3ef
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-06-29 13:41:51 +0300

    [CB/fix]: Update structure of index-file.

--- src/index.js
+++ src/index.js
@@ -1,53 +1,50 @@
-import * as PIXI from 'pixi.js'
-import { Application } from '@pixi/app'
-import { AppLoaderPlugin } from '@pixi/loaders';
+import * as PIXI from 'pixi.js';
 import { gsap } from "gsap";
 import { PixiPlugin } from "gsap/PixiPlugin";
-import Preloader from "./js/PreloaderScene.js"
+import Preloader from "./js/PreloaderScene.js";
 import Main from './js/MainScene.js';
 
-const size = [540, 960]
+const size = [540, 960];
 
-Application.registerPlugin(AppLoaderPlugin);
-
-const app = new Application({
+const app = new PIXI.Application({
     width: size[0],
     height: size[1],
     resolution: window.devicePixelRatio,
-    backgroundColor: 0xEDE8E5
-})
-let ratio = size[0] / size[1]
-let renderer = PIXI.autoDetectRenderer(size[0], size[1], null)
-document.body.appendChild(app.view)
-resize()
+    background: 0xEDE8E5
+});
 
-gsap.registerPlugin(PixiPlugin)
+document.body.appendChild(app.view);
 
+gsap.registerPlugin(PixiPlugin);
 
-const prld = await new Preloader(app).startLoad()
+const preloader = new Preloader(app);
+await preloader.startLoad();
 
-const game = new Main(app)
+const game = new Main(app);
 
-app.loader.load(() => game.startGame() )
+game.startGame();
 
 function resize() {
+    const ratio = size[0] / size[1];
+    
     if (window.innerWidth / window.innerHeight >= ratio) {
-        let ancho = ~~(window.innerHeight * ratio)
-        let alto= window.innerHeight
-        app.view.style.position = 'absolute'
-        app.view.style.width = ancho + 'px'
-        app.view.style.height = alto + 'px'
-        app.view.style.left = ~~((window.innerWidth-ancho)/2) + 'px'
-        app.view.style.top = '0px'
-        
+        const ancho = ~~(window.innerHeight * ratio);
+        const alto = window.innerHeight;
+        app.view.style.position = 'absolute';
+        app.view.style.width = ancho + 'px';
+        app.view.style.height = alto + 'px';
+        app.view.style.left = ~~((window.innerWidth - ancho) / 2) + 'px';
+        app.view.style.top = '0px';
     } else {
-        let ancho = window.innerWidth
-        let alto = ~~(window.innerWidth / ratio)
-            app.view.style.position = 'absolute'
-            app.view.style.width = ancho + 'px'
-            app.view.style.height = alto + 'px'
-            app.view.style.left = 0 + 'px'
-            app.view.style.top = (window.innerHeight/2-(alto/2)) + 'px'
+        const ancho = window.innerWidth;
+        const alto = ~~(window.innerWidth / ratio);
+        app.view.style.position = 'absolute';
+        app.view.style.width = ancho + 'px';
+        app.view.style.height = alto + 'px';
+        app.view.style.left = '0px';
+        app.view.style.top = (window.innerHeight / 2 - alto / 2) + 'px';
     }
 }
-window.onresize = resize
+
+resize();
+window.onresize = resize;
