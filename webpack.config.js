feb412ecdd9c562447bc5a59a2366be8d0f76215
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-08-11 17:12:28 +0300

    [CB/feat]: Add publicPath rule for webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   output: {
     filename: "bundle.min.js",
     path: path.resolve(__dirname, 'dist/'),
-    clean: true
+    clean: true,
+    publicPath: "",
   },
   module: {
     rules: [
